ebde43c7322c7d5eba07dbe91e7937b1bdc8e765
pughdr <david.pugh@kaust.edu.sa>
2020-07-29 12:43:17 +0300

    Removed unused Horovod build vars; prefer source over .

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -1,13 +1,9 @@
 #!/bin/bash --login
 
-# set relevant build variables for horovod
-export ENV_PREFIX=$PWD/env
-export HOROVOD_CUDA_HOME=$CUDA_HOME
-export HOROVOD_NCCL_HOME=$ENV_PREFIX
-export HOROVOD_GPU_ALLREDUCE=NCCL
-export HOROVOD_GPU_BROADCAST=NCCL
+set -e
 
 # create the conda environment
+export ENV_PREFIX=$PWD/env
 conda env create --prefix $ENV_PREFIX --file environment.yml --force
 conda activate $ENV_PREFIX
-. postBuild
+source postBuild
