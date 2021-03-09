f11cdc33d3fb427697f8d796365ffaebf88faa74
pughdr <david.pugh@kaust.edu.sa>
2021-03-09 12:15:58 +0300

    Updated the Conda env build script

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -3,7 +3,8 @@
 set -e
 
 # create the conda environment
-export ENV_PREFIX=$PWD/env
-conda env create --prefix $ENV_PREFIX --file environment.yml --force
+PROJECT_DIR=$PWD
+ENV_PREFIX=$PROJECT_DIR/env
+conda env create --prefix $ENV_PREFIX --file $PROJECT_DIR/environment.yml --force
 conda activate $ENV_PREFIX
 source postBuild
