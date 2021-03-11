2513361dca25b26d7183a72a246aaddf2607494d
pughdr <david.pugh@kaust.edu.sa>
2021-03-11 15:02:11 +0300

    Build scripts for conda

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -1,10 +1,11 @@
 #!/bin/bash --login
 
+# entire script fails if a single command fails
 set -e
 
 # create the conda environment
-PROJECT_DIR=$PWD
-ENV_PREFIX=$PROJECT_DIR/env
-conda env create --prefix $ENV_PREFIX --file $PROJECT_DIR/environment.yml --force
+PROJECT_DIR="$PWD"
+ENV_PREFIX="$PROJECT_DIR"/env
+conda env create --prefix $ENV_PREFIX --file "$PROJECT_DIR"/environment.yml --force
 conda activate $ENV_PREFIX
 source postBuild
