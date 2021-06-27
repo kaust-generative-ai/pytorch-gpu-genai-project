bf0aabd2598ac9691fe74e9fd43163512944c4bf
davidrpugh <drpugh@protonmail.com>
2021-06-28 02:18:04 +0300

    Updated versions of key deps.

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -6,6 +6,4 @@ set -e
 # create the conda environment
 PROJECT_DIR="$PWD"
 ENV_PREFIX="$PROJECT_DIR"/env
-conda env create --prefix $ENV_PREFIX --file "$PROJECT_DIR"/environment.yml --force
-conda activate $ENV_PREFIX
-source postBuild
+mamba env create --prefix $ENV_PREFIX --file "$PROJECT_DIR"/environment.yml --force
