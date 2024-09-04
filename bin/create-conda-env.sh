804430fe705925842253fed2f4ec59f08fe62278
davidrpugh <david.pugh@kaust.edu.sa>
2024-09-04 13:20:37 +0300

    --yes instead of --force

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -6,4 +6,4 @@ set -e
 # create the conda environment
 PROJECT_DIR="$PWD"
 ENV_PREFIX="$PROJECT_DIR"/env
-mamba env create --prefix $ENV_PREFIX --file "$PROJECT_DIR"/environment.yml --force
+mamba env create --prefix $ENV_PREFIX --file "$PROJECT_DIR"/environment.yml --yes
